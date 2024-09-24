b399c0ce486e5c544f7915d186d2ec50900f6c91
Cesi20 <ctitoh@autonoma.edu.pe>
2024-09-24 00:39:42 -0500

    v1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 function Greeting(){
-    const hola = 'Ya vamos a dormir pe'
+    const hola = 'QUE WEBADA'
     return <h1>{hola}</h1>
 }
 
